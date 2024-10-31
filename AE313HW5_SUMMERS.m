79e9570766695a418e022f17c03b6e18f2d93b1c
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2024-10-30 21:06:06 -0400

    Finished

--- AE313HW5_SUMMERS.m
+++ AE313HW5_SUMMERS.m
@@ -32,35 +32,76 @@ nMag = sqrt(sum(N.^2));
 dMag = sqrt(sum(D.^2));
 
 v = cell(1);
-hMag = zeros(1);
-eMag = zeros(1);
+hMag = zeros(1, 3);
+eMag = zeros(1, 3);
+thetaList = zeros(1, 3);
+raanList = zeros(1, 3);
+incList = zeros(1, 3);
+apList = zeros(1, 3);
 
 for i = 1:3
     vI = sqrt(m / (nMag * dMag)) * (cross(D, r{i})./rMag(i) + S);
     v{i} = vI;
 
+    % Specific Angular Momentum
     hI = cross(r{i}, v{i});
     hMag(i) = sqrt(sum(hI.^2));
-
+    
+    % Eccentricity 
     eI = (cross(r{i}, v{i}) / m) - (r{i} / rMag(i));
     eMag(i) = sqrt(sum(eI.^2));
     
+    % Mean anomoly
+    theta = acos(dot(r{i}, eI) / (rMag(i) * eMag(i)));
+    if dot(r{i}, v{i}) < 0
+        % angle correction due to unknown quadrent
+        theta = 2 * pi - theta;
+    end
+    thetaList(i) = (180 / pi) * theta;
+    
+    % Node line needed for angles
+    nodeLine = cross([0,0,1], hI);
+    nodeMag = sqrt(sum(nodeLine.^2));
+    
+    % Right Ascension of the Ascending Node
+    ra = acos(nodeLine(1) / nodeMag); 
+    if nodeLine(2) < 0
+        % angle correction due to unknown quadrent
+        ra = 2 * pi - ra;
+    end
+    ra = (180 / pi) * ra;
+
+    % Inclination
+    inc = acos(dot(hI, [0, 0, 1]) / hMag(i));
+    inc = (180 / pi) * inc;
+    
+    % Argument of Perigee
+    ap = acos(dot(nodeLine, eI) / (nodeMag * eMag(i)));
+    if eI(3) < 0
+        % angle correction due to unknown quadrent
+        ap = 2 * pi - ap;
+    end
+    ap = (180 / pi) * ap;
+
+    % Add each angle to list
+    raanList(i) = ra;
+    incList(i) = inc;
+    apList(i) = ap;
     
 end
 
-
-
-
-
-
-
-
-
 fprintf("+-+-+ OUTPUTS +-+-+")
-fprintf("\n Velocity vectors: ")
+fprintf("\n Velocity vectors (km/s): ")
 disp(v);
-fprintf("\n h magnitude: ")
+fprintf("\n h magnitude (km^2/s): ")
 disp(hMag);
 fprintf("\n e magnitude: ")
 disp(eMag);
-
+fprintf("\n Mean anomoly: ")
+disp(thetaList);
+fprintf("\n RAAN: ")
+disp(raanList);
+fprintf("\n INC: ")
+disp(incList);
+fprintf("\n AP: ")
+disp(apList);
